6f8d536c450dd4b78e81510ccb807932ad4f4a8f
Robert <robert.balicki@gmail.com>
2018-06-17 23:03:31 -0400

    move things around

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,6 @@
 // @flow
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
 import StateProvider from 'src/components/Providers/StateProvider';
 import { type TimeOfDay } from 'src/components/Contextual/StyleContext/TimeOfDay';
@@ -7,19 +8,40 @@ import Background from 'src/components/Contextual/Background';
 import App from 'src/components/App';
 import { UpdateContext } from './components/Contextual/StyleContext';
 
-type TimeOfDayUpdater = (TimeOfDay) => void;
+import appleTouchIcon from './components/App/Head/apple-touch-icon.png';
+import favicon32 from './components/App/Head/favicon-32x32.png';
+import favicon16 from './components/App/Head/favicon-16x16.png';
+// N.B. this is apparently a bug. import/extensions doesn't understand query params...
+/* eslint-disable import/extensions */
+// $FlowFixMe - TODO figure out how to make flow recognize these
+import safariPinnedTab from './components/App/Head/safari-pinned-tab.svg?external';
+/* eslint-enable import/extensions */
+// $FlowFixMe
+import favicon from './components/App/Head/favicon.ico';
 
-export default () => (<StateProvider initialValue="DAY">
-  {
-    (time: TimeOfDay, setTimeOfDay: TimeOfDayUpdater) =>
-      (<UpdateContext call={context => context.setTimeOfDay(time)}>
-        <Background />
-        <App
-          toggleTimeOfDay={
-            () => setTimeOfDay(time === 'DAY' ? 'NIGHT' : 'DAY')
-          }
-          isDay={time === 'DAY'}
-        />
-      </UpdateContext>)
-  }
-</StateProvider>);
+type TimeOfDayUpdater = (TimeOfDay) => void;
+export default () => (<Fragment>
+  <Helmet>
+    <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+    <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+    <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <link rel="shortcut icon" href={favicon} />
+    <meta name="theme-color" content="#ffffff" />
+    <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
+  </Helmet>
+  <StateProvider initialValue="DAY">
+    {
+      (time: TimeOfDay, setTimeOfDay: TimeOfDayUpdater) =>
+        (<UpdateContext call={context => context.setTimeOfDay(time)}>
+          <Background />
+          <App
+            toggleTimeOfDay={
+              () => setTimeOfDay(time === 'DAY' ? 'NIGHT' : 'DAY')
+            }
+            isDay={time === 'DAY'}
+          />
+        </UpdateContext>)
+    }
+  </StateProvider>
+</Fragment>);
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,8 @@
 // @flow
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import { Helmet } from 'react-helmet';
 import TimeOfDayToggler from './TimeOfDayToggler';
 import Routes from './Routes';
-
-import appleTouchIcon from './Head/apple-touch-icon.png';
-import favicon32 from './Head/favicon-32x32.png';
-import favicon16 from './Head/favicon-16x16.png';
-// N.B. this is apparently a bug. import/extensions doesn't understand query params...
-/* eslint-disable import/extensions */
-// $FlowFixMe - TODO figure out how to make flow recognize these
-import safariPinnedTab from './Head/safari-pinned-tab.svg?external';
-/* eslint-enable import/extensions */
-// $FlowFixMe
-import favicon from './Head/favicon.ico';
 
 type ToggleTimeOfDay = () => void;
 
@@ -27,21 +15,13 @@ const AppContainer = styled.div`
   width: 100%;
   max-width: 970px;
   margin: 80px auto 80px;
+  transition: 400ms linear all;
 `;
 
 export default ({
   toggleTimeOfDay,
   isDay,
 }: AppProps) => (<Fragment>
-  <Helmet>
-    <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-    <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
-    <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <link rel="shortcut icon" href={favicon} />
-    <meta name="theme-color" content="#ffffff" />
-    <link rel="mask-icon" href={safariPinnedTab} color="#5bbad5" />
-  </Helmet>
   <TimeOfDayToggler
     isDay={isDay}
     toggleTimeOfDay={toggleTimeOfDay}
